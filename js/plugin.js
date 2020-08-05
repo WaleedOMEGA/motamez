71e1a813894d7eda58a22a141ed2cf698dd51851
Waleed OMEGA <waleedomega@hotmail.com>
2020-08-05 14:04:23 +0200

    Add files via upload

---
+++ js/plugin.js
@@ -0,0 +1,54 @@
+jQuery(function($) {'use strict',
+    //wow
+    new WOW().init();
+    //carousel
+    $(function(){
+		$('#home').carousel({
+			interval: 8000
+		});
+	});
+    //prettyphoto
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});
+    // accordian
+    $('.accordion-toggle').on('click', function(){
+		$(this).closest('.panel-group').children().each(function(){
+		$(this).find('>.panel-heading').removeClass('active');
+		 });
+
+	 	$(this).closest('.panel-heading').toggleClass('active');
+	});
+    // portfolio filter
+	$(window).load(function(){'use strict';
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+		var $portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : '.portfolio-item',
+			layoutMode : 'fitRows'
+		});
+		
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
+    // Contact form
+	var form = $('#main-contact-form');
+	form.submit(function(event){
+		event.preventDefault();
+		var form_status = $('<div class="form_status"></div>');
+		$.ajax({
+			url: $(this).attr('action'),
+
+			beforeSend: function(){
+				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> يتم إرسال الرسالة...</p>').fadeIn() );
+			}
+		}).done(function(data){
+			form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
+		});
+	});
+});
